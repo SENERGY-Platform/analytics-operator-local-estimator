e28c0192acf7036c9541fe272851df4e5e4fbddf
TZ <zsco@users.noreply.github.com>
2020-11-11 11:34:21 +0100

    implement prediction

--- main.py
+++ main.py
@@ -11,12 +11,43 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from datetime import datetime,timedelta, timezone
 
 from senergy_local_analytics import App, Input, Output
+from skmultiflow.trees import HoeffdingTreeRegressor
+import numpy as np
+from dateutil.relativedelta import *
+
+ht_reg = HoeffdingTreeRegressor()
+
+
+def train(value, current_timestamp):
+    x, y = np.array([[current_timestamp, 1]]), np.array([value])
+    ht_reg.partial_fit(x, y)
+
+
+def predict(target_timestamp):
+    pred = ht_reg.predict(np.array([[target_timestamp, 1]]))
+    return pred[0]
 
 
 def process(inputs: list[Input]):
-    return Output(True, {})
+    today = datetime.utcnow().date()
+    eoy = datetime(today.year, 12, 31)
+    eom = datetime(today.year, today.month, 1) + relativedelta(months=1)
+    eod = datetime(today.year, today.month, today.day) + timedelta(days=1)
+
+    value = 0
+    dt = datetime.now()
+    timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
+    for inp in inputs:
+        if inp.name == "value" and inp.current_value is not None:
+            value = inp.current_value
+    train(value, timestamp)
+    pred_day = predict(eod.replace(tzinfo=timezone.utc).timestamp())
+    pred_month = predict(eom.replace(tzinfo=timezone.utc).timestamp())
+    pred_year = predict(eoy.replace(tzinfo=timezone.utc).timestamp())
+    return Output(True, {"pred_day": pred_day, "pred_month": pred_month, "pred_year": pred_year})
 
 
 if __name__ == '__main__':
--- test/test.py
+++ test/test.py
@@ -11,20 +11,43 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from datetime import datetime, timezone, timedelta
 import unittest
-
-from senergy_local_analytics import Input
-
+import numpy as np
+from dateutil.relativedelta import *
 import main
 
 
 class TestMainMethods(unittest.TestCase):
 
-    def test_process(self):
-        input1 = Input("value1")
-        input2 = Input("value2")
-        input1.current_value = 1
-        input2.current_value = 3
-        output = main.process([input1, input2])
-        self.assertTrue(output.send)
-        self.assertEqual({'sum': 4}, output.values)
+    def test_predict(self):
+        n_samples = 0
+        max_samples = 744
+        data = np.random.randint(6, size=max_samples)
+        target_value = np.sum(data)
+        today = datetime.utcnow().date()
+        dt = datetime(today.year, 1, 1)
+        target = datetime(today.year, 2, 1)
+        target_timestamp = target.replace(tzinfo=timezone.utc).timestamp()
+        val = 0
+        while n_samples < max_samples:
+            val = val + data[n_samples]
+            timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
+            x, y = np.array([[timestamp, 1]]), np.array([val])
+            print(x, y)
+            main.train(val, timestamp)
+            pred = main.predict(target_timestamp)
+            print(pred)
+            print(pred / target_value)
+            print("*******************")
+            dt = dt + timedelta(hours=1)
+            n_samples += 1
+
+    def test_date(self):
+        today = datetime(2013, 12, 31)
+        eoy = datetime(today.year, 12, 31)
+        eom = datetime(today.year, today.month, 1)+relativedelta(months=1)
+        eod = datetime(today.year, today.month, today.day)+timedelta(days=1)
+        print(eoy)
+        print(eom)
+        print(eod)
